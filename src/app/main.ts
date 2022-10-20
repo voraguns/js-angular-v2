b8c55df7315844176ab847812a180c46bf79bc7d
Voragun <voragun2543@gmail.com>
2022-10-20 17:37:48 +0700

    add red and blue for show and reset score

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,7 +2,26 @@ import { Component } from "@angular/core"
 
 @Component({
    selector: "main",
-   template: "<button>The Main Button</button>"
+   template: `
+               <button (click)="nextBlue()">blue</button>
+               <button (click)="nextRed()">red</button>
+               <button (click)="reset()">Reset</button>
+               <h3>Blue : {{ blue }}</h3>
+               <h3>Red : {{ red }}</h3>
+              `
 })
 			
-export class Main { }
+export class Main {
+   blue = 0
+   red = 0
+   nextRed() {
+     this.red++
+   } 
+   nextBlue(){
+     this.blue++
+   }
+   reset(){
+     this.blue = 0
+     this.red = 0
+   }
+}
