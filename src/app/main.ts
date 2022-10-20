5ae7acdf4984d719d099186945d9a0425732b82b
Voragun <voragun2543@gmail.com>
2022-10-20 18:08:12 +0700

    Status application

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,15 +3,17 @@ import { Component } from "@angular/core"
 @Component({
    selector: "main",
    template: `
-               <button (click)="addLove()">Love</button>
-               <button (click)="addHate()">Hate</button>
-               <p *ngFor="let e of data">{{ e }} </p>
+               <button (click)="next()">{{ status[current]  }}</button>
               `
 })
 			
 export class Main {
-   // data = [ ]
-   data: Array<string> = [ ]
-   addLove() { this.data.push("Love") }
-   addHate() { this.data.push("Hate") }
+   current = 0
+   status = [ "Single", "Married", "Divorce" ]
+   next() {
+     this.current++
+     if (this.current >= this.status.length) {
+        this.current = 0
+     }
+   }
 }
